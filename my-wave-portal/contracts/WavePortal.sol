4fc7734a0ca686aacfc210c5555d6f85094606fd
Charles M Dellinger <charles.dellinger@gmail.com>
2022-04-06 10:52:57 -0400

    added increment and getter functions to contract and updated tests to include them

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    
     constructor() {
         console.log("In the end, I will say hi!");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
