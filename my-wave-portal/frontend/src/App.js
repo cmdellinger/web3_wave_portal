258a1f876e11bae470bc0675e1cc2d45ea5a8b5a
Charles M Dellinger <charles.dellinger@gmail.com>
2022-04-19 22:49:30 -0400

    added wave function and linked frontend to backend

--- my-wave-portal/frontend/src/App.js
+++ my-wave-portal/frontend/src/App.js
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
 import "./App.css";
+import abi from "./utils/WavePortal.json";
+
+require('dotenv').config();
+
 
 const App = () => {
   /*
   * Just a state variable we use to store our user's public wallet.
   */
   const [currentAccount, setCurrentAccount] = useState("");
+  /**
+  * Create a variable here that holds the contract address after you deploy!
+  */
+  const contractAddress = "0xC4779d82B867D0c80de9238D1Bba68faEC42A61B";
+  /**
+  * Create a variable here that references the abi content!
+  */
+  const contractABI = abi.abi;
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -61,6 +74,40 @@ const App = () => {
   }
 
   /*
+  * This runs our smart contract's wave function
+  */
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        /*
+        * Execute the actual wave from your smart contract
+        */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  /*
   * This runs our function when the page loads.
   */
   useEffect(() => {
@@ -78,7 +125,7 @@ const App = () => {
           I am Charley and I am learning solidity and React. Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={null}>
+        <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
 
